63e390ce67fe7340cfb78ad121d931a368796c09
dwkoo <dwkoo@humaxdigital.com>
2021-07-12 16:01:32 +0900

    Remove MusicTrackList (MusicTrackList by Kotlin is moved to MusicTrackList git repository

--- MusicTrackList/app/src/main/java/dw/koo/android/homework/musictracklist/adapter/IOnHolderListener.kt
+++
@@ -1,7 +0,0 @@
-package dw.koo.android.homework.musictracklist.adapter
-
-import android.view.View
-
-interface IOnHolderListener {
-    fun onHolderClicked(position: Int, view: View?)
-}
--- MusicTrackList/app/src/main/java/dw/koo/android/homework/musictracklist/data/FragmentType.kt
+++
@@ -1,5 +0,0 @@
-package dw.koo.android.homework.musictracklist.data
-
-enum class FragmentType {
-    MAIN_FRAGMENT, FAVORITE_TRACK_LIST_FRAGMENT, NONE
-}
